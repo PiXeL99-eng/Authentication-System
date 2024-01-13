0bfd71c6ef3a945282ac1cd0765d19002473636a
Sayantan Kundu <sayantankundu02@gmail.com>
2024-01-14 00:22:13 +0530

    Home page ready

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Container, Stack, Heading, Button, Avatar} from '@chakra-ui/react'
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    console.log("check")
+  }
+
   return (
-    <div>Home</div>
+
+    <>
+      <Container maxW="100vw" height="100vh" bg='#14151e'>
+        <Container maxW="2xl" centerContent height={"100vh"}>
+          <Stack spacing={4} direction='column' my={"auto"} minWidth={"50%"}>
+
+            <Stack direction='column' color="#fffffc" spacing={4}>
+
+              {/* <Avatar bg='#0bc5ea' icon={<AiOutlineUser fontSize='1.5rem' />} /> */}
+              <Avatar bg='#0bc5ea' mx={"auto"}/>
+              <Heading as='h1' size='xl' textAlign={"center"}>
+                Hi Sayan
+              </Heading>
+
+              <Button
+                // isLoading
+                loadingText='Loading'
+                colorScheme='cyan'
+                spinnerPlacement='end'
+                type="submit"
+                width="full"
+                color="black"
+                marginY="2"
+                onClick={handleClick}
+              >
+                Log out
+              </Button>
+
+            </Stack>
+          </Stack>
+        </Container>
+      </Container>
+    </>
   )
 }
 
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,5 @@
 import Home from './Home';
-import Login from './Login';
+import Login from './Login'
 import Signup from './Signup';
 
 export {
